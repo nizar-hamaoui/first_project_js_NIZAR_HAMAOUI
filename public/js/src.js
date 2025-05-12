bddd67f0dbcaa6e044d4cb27df120fb1e570ad65
ame <glomark779@gmail.com>
2025-05-12 16:00:50 +0100

    edit log in and reset password

--- public/js/src.js
+++ public/js/src.js
@@ -1,4 +1,4 @@
-let chose = prompt("What do you want " + "signing_up" + " or " + "logging_in" + " or " + "changing_the_password" + "?");
+var chose = prompt("What do you want \n signing_up " + "?");
 
 class users {
     constructor(username, email,age, money, password){
@@ -14,16 +14,16 @@ class users {
     }
 }
 
-
+let marck = new users(username, email, age1, 1000, password)
 
 
 if (chose == "exit") {
     alert("Exiting the application.");
     while(true) {
-        chose = prompt("What do you want \n signing_up  or \n logging_in  or  \n changing_the_password" + "?");
+        chose = prompt("What do you want \n signing_up " + "?");
         break;
     }
-}else if (chose == "signing_up") {
+}else if (chose === "signing_up") {
     alert("You chose to sign up.");
     var username = prompt("Enter your username:").trim();
     username = username.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
@@ -45,25 +45,17 @@ if (chose == "exit") {
             alert("Email must be at least 10 characters long.");
        
         } 
-    else if (/\s/.test(email)) {
+      else if (/\s/.test(email)) {
             alert("Email must not contain spaces.");
      } 
      else if (email.split('@').length !== 2) {
         alert("Email mafihch  '@' ."); 
     }
-    else if (!/^[^@\s]+@[^@\s]+\.[^@\s]+$/.test(email)) {
-        alert("Email must be in a valid format.");
-    }
-
-    
-    if (users.email.includes(email)) {
-    alert("This email is already in use. Please use a different email.");
-    } else {
-    alert("Email is unique and saved.");
-    users.email.push(email); 
-
-    
-    }
+if (marck.email === email) {
+     alert("This email is already in use. Please use a different email.");
+} else {
+     alert("Email is unique and saved.");
+}
     var age1 = prompt("Enter your age :").trim();
     if (isNaN(age1)) {
         alert("Age baghi only have numbers.");
@@ -73,7 +65,7 @@ if (chose == "exit") {
         } else {
         alert("Age is correct and saved.");
     }
-    let password = prompt("Enter you password : ").trim();
+    var password = prompt("Enter you password : ").trim();
     if (/\s/.test(password)){
         alert("3chiri fiha espace hayadha ")
         password = prompt("Enter your password:").trim();
@@ -90,10 +82,54 @@ if (chose == "exit") {
         alert("Passwords do not match. You are blocked.");
     } else {
         alert("Password confirmed and saved.");
+        
     }
-   
+    chose = prompt("What do you want \n logging_in  or  \n changing_the_password" + "?");
+    if (chose === "logging_in"){
+        var enteremail = prompt("Enter your email : ")
+        if (enteremail == marck.email) {
+          let conpass = prompt("enter your password : ")
+          if (conpass == marck.password) {
+            alert("log in sueccessfully")
+            chose = prompt("What do you want  \n changing_the_password" + "?");
+            if (chose == "changing_the_password") {
+                let emailInput = prompt("Enter your email:");
+                if (emailInput === marck.email) {
+                    let oldPasswordInput = prompt("Enter your old password:");
+                    if (oldPasswordInput === marck.password) {
+                        let newPassword = prompt("Enter your new password:").trim();
+                        if (newPassword.includes(" ")) {
+                            alert("Password should not have spaces.");
+                        } else if (newPassword.length < 7) {
+                            alert("Password should be at least 7 characters long.");
+                        } else if (!/[@#\-+*/]/.test(newPassword)) {
+                            alert("Password should have at least one special character like @, #, -, +, *, or /.");
+                        } else {
+                            let confirmNewPassword = prompt("Type your new password again:").trim();
+                            if (confirmNewPassword === newPassword) {
+                                marck.password = newPassword;
+                                alert("Password changed successfully!");
+                            } else {
+                                alert("Passwords do not match. Try again.");
+                            }
+                        }
+                    } else {
+                        alert("Old password is wrong.");
+                    }
+                } else {
+                    alert("Email does not match.");
+                }
+            }
+            alert(`your money in bank is : ${marck.money}$ `)
+          }else{
+            alert("password ghalat !!")
+          }
+         
 
+        }else{
+          alert("your email khataaa")
+        }
+      }
 
 
 }
-// let marck = new users(username, email)
